e3b41585c2b834bd6e917738259256f4bb82bbed
Nathan Chilton <nathan.chilton@clinicalresearch.io>
2023-04-19 09:56:21 -0500

    change quotation marks

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 curl -X POST \
   'https://api.github.com/repos/nathanchilton/argo-examples/actions/workflows/test.yml/dispatches' \
   --header 'Accept: application/vnd.github+json' \
-  --header 'Authorization: token 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' \
+  --header "Authorization: token $GHA_TOKEN" \
   --header 'Content-Type: application/json' \
   --data-raw '{
   "ref": "master", "inputs": {
